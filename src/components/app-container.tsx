b12e6f80940453e52c0bf72aea002595212196ec
Denys Skorov <denys.s@sterl.in>
2024-07-30 16:13:49 +0300

    update stores in the components

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -8,17 +8,17 @@ import { TaskList } from './task-list'
 import { ThemeToggle } from './theme-toggle'
 
 export const AppContainer = observer(() => {
-    const store = useStore()
+    const { theme } = useStore()
 
     useEffect(() => {
-        document.body.setAttribute('data-mode', store.theme.themeMode)
-    }, [store.theme.themeMode])
+        document.body.setAttribute('data-mode', theme.themeMode)
+    }, [theme.themeMode])
 
     return (
         <div className="max-w-screen-md mx-auto p-3">
             <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                    <img src={Logo} alt="logo"></img>
+                    <img src={Logo} alt="logo"/>
                     <div className="text-xl text-primary">Todo</div>
                 </div>
                 <ThemeToggle />
